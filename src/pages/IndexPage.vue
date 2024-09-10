ca05d124db7603f9f0af6975b27735aa4d14c9fa
M.R Khodaee <mrkhodaee24@gmail.com>
2024-09-10 08:55:57 +0330

    change base url api to localhost port 8000

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,73 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page class="">
+    <q-input
+      placeholder="email"
+      label="Email"
+      type="email"
+      class="q-mt-lg"
+      rounded
+      outlined
+      v-model="username"
+    />
+    <input type="text" />
+    <q-input
+      placeholder="password"
+      label="Password"
+      class="q-mt-lg"
+      rounded
+      outlined
+      v-model="password"
+    />
+    <q-input
+      placeholder="confirm password"
+      label="Confirm Password"
+      class="q-mt-lg"
+      rounded
+      outlined
+      v-model="confirmPassword"
+    />
+    <q-btn
+      class="full-width q-mt-md q-py-sm bg-purple-8"
+      color="primary"
+      rounded
+      @click="register"
+      >Register</q-btn
     >
+
+    {{ username }}
   </q-page>
 </template>
 
 <script setup>
+import { api } from "src/boot/axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const username = ref("");
+const password = ref("");
+const confirmPassword = ref("");
+
+function register() {
+  if (password.value === confirmPassword.value) {
+    api
+      .post("api/register", {
+        email: username.value,
+        password: password.value,
+      })
+      .then((r) => {
+        console.log(r.data);
+        if (r.data.status) {
+          console.log(1);
+
+          router.push("/login");
+        } else {
+          alert("prablum");
+        }
+      });
+  }
+}
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
